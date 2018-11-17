5ec661dfc5fbdfcd9e28789e97b10fdc4d374d44
aaronysl <ysl158233@163.com>
2018-11-17 19:08:41 +0800

    手动实现登录接口（可扩展性较差）

--- mido_mall/mido_mall/apps/users/views.py
+++ mido_mall/mido_mall/apps/users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_jwt.settings import api_settings
+from django.db.models import Q
+
 from .models import User
 from . import serializers
 # url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
@@ -41,3 +44,33 @@ class MobileCountView(APIView):
 # url(r'^users/$', views.UserView.as_view()),
 class UserView(CreateAPIView):
     serializer_class = serializers.CreateUserSerializer
+
+
+#手动实现登录接口，可扩展性较差
+class LoginApiView(APIView):
+    def post(self,request):
+        data=request.data
+        username=data.get('username','')
+        password=data.get('password','')
+
+        users =User.objects.filter(Q(username=username) | Q(mobile=username))       #queryset要么有一个元素，要么没有元素
+        if users:
+            #用户存在
+            user=users[0]
+            #校验密码
+            if user.check_password(password):
+                #颁发token
+                # 生成jwt token
+                jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+                jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+                payload = jwt_payload_handler(user)
+                token = jwt_encode_handler(payload)
+
+                return Response({
+                    'username':user.username,
+                    'user_id':user.id,
+                    'token':token
+                })
+            return Response({'message':'用户名或密码错误'},status=400)
+
